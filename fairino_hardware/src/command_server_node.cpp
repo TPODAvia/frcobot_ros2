5f031557f1662b7ab5bd3efd71f9ed9e052648fb
TPODAvia <thepowerofdarknes2000@gmail.com>
2025-04-14 21:52:18 +0300

    Port hardware V3.0

--- fairino_hardware/src/command_server_node.cpp
+++ fairino_hardware/src/command_server_node.cpp
@@ -1,5 +1,6 @@
 #include "fairino_hardware/command_server.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "libfairino/include/robot.h"
 
 int main(int argc, char *argv[]){
     //该main函数用于创建简化指令客户端的app
@@ -10,7 +11,7 @@ int main(int argc, char *argv[]){
     mulexecutor.add_node(command_server_node);
     //创建非实时状态反馈获取节点
     auto robot_state_node = std::make_shared<robot_recv_thread>("fr_state_brodcast");
-    mulexecutor.add_node(robot_state_node);//状态反馈节点加入执行器中
+    mulexecutor.add_node(robot_state_node);//状态反馈节点加入执行器
     mulexecutor.spin();
     rclcpp::shutdown();
     return 0;
